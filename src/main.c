aefda55918f8bacccf771678c3a6b6240ab2d73a
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-03-03 16:21:11 +0000

    ✨ Main loop

--- src/main.c
+++ src/main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include "../include/os_kernel.h"
 #include "../include/display.h"
 
 int main() {
+    bool running = true;
     printf("\033[2J"); // Limpa a tela no início
     boot();
     sleep(5);
-    draw_top_UI();
-    printf("\n\n--- FEATURE PHONE OS ---\n");
-    printf("Pressione Enter para sair...\n");
-    getchar();
+
+    while(running){
+        printf("\033[H");
+        draw_top_UI();
+        printf("\033[5;1H");
+        printf("--- MAIN MENU ---\n");
+        printf("1. Messages\n");
+        printf("2. Contacts\n");
+        printf("0. Power Off\n");
+        usleep(100000);
+    }
+
     return 0;
 }

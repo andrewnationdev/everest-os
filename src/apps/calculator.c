ed924df684a13d86d9633f2277be946113e86fe9
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-03-14 19:28:23 +0000

    ✨ Add calculator app

--- src/apps/calculator.c
+++ src/apps/calculator.c
@@ -28,7 +28,53 @@ int calc_app(){
 
 void render_calc(){
     app_init();
+
+    float x, y, result;
+
     printf("--- CALCULATOR ---\n");
-    //...code goes here
+    printf("First number: ");
+    scanf("%f", &x);
+    printf("Second number: ");
+    scanf("%f", &y);
+
+    if(x || x == 0.0){
+        if(y || y == 0.0){
+            printf("--- Operations ---\n");
+            printf("1-Sum\n2-Divide\n3--Multiply\n4-Subtract\n0-Delete\n9-Exit\n> ");
+
+            int op;
+
+            scanf("%d", &op);
+
+            switch(op){
+                case 1: //Sum
+                    result = x + y;
+                    break;
+                case 2: //Divide
+                    if(y == 0.0) return; //Division by zero
+                    result = x / y;
+                    break;
+                case 3: // Multiply
+                    result = x * y;
+                    break;
+                case 4: //Subtract
+                    result = x - y;
+                    break;
+                case 9:
+                    return;
+                    break;
+                default:
+                    x = 0.0;
+                    y = 0.0;
+                    result = 0.0;
+                    break;
+            }
+
+            printf("Result: %f\n", result);
+        }
+    }
+
+    usleep(5000000);
+
     fflush(stdout);
 }
